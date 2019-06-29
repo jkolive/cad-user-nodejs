e3e6393222c4f0c76f4777ff778105327b236769
Jackson de Oliveira <mr.jkolive@gmail.com>
2019-06-28 23:55:37 -0300

    Arquivos iniciais

---
+++ src/database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
+module.exports = mongoose;
