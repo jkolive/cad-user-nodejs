717e43c940fec44d08759e7561bcbb4e5b46b0a5
Jackson de Oliveira <mr.jkolive@gmail.com>
2019-06-29 20:04:15 -0300

    Restrutução das pastas

--- src/app/controllers/authControllers.js
+++ src/app/controllers/authControllers.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const authConfig = require('../config/auth.json')
+const authConfig = require('../../config/auth.json')
 
 const router = express.Router();
 
